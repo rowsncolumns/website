df2319fb7c50c953c55c74b9123f745be7a78517
Vinay M <rmdort@gmail.com>
2023-05-26 10:59:34 +0800

    Update canvas

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   webpack (config) {
     config.externals.push('canvas')
     return config
+  },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/canvas',
+      ],
+    },
   }
 }
 
