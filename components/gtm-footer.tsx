73fc92ed14d20d564495a8ddda080d07d2b232a3
Vinay M <rmdort@gmail.com>
2023-06-27 19:37:09 +0800

    Add ids

--- components/gtm-footer.tsx
+++ components/gtm-footer.tsx
@@ -1,7 +1,7 @@
 import Script from "next/script";
 
 export const GTMFooter = () => (
-  <Script>
+  <Script id="gtm-footer">
     {`<!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWFSTRX"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
--- components/gtm-head.tsx
+++ components/gtm-head.tsx
@@ -1,7 +1,7 @@
 import Script from "next/script";
 
 export const GTMHead = () => (
-  <Script>
+  <Script id="gtm-head">
     {`<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
