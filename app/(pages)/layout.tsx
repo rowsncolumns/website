25708bea35a16ad3082b2cec2fc5d657632a33e4
Vinay M <rmdort@gmail.com>
2023-06-28 00:35:23 +0800

    Update layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -10,6 +10,7 @@ import { HamburgerMenuIcon } from "@rowsncolumns/icons";
 import { Analytics } from "@vercel/analytics/react";
 import { GTMHead } from "@/components/gtm-head";
 import { GTMFooter } from "@/components/gtm-footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin-ext"] });
 
@@ -21,7 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <GTMHead />
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MWFSTRX');`,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <header className="border-b border-site-border">
           <div className="container items-center">
@@ -177,7 +186,13 @@ export default function RootLayout({
           </div>
           <Analytics />
         </footer>
-        <GTMFooter />
+        <Script
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWFSTRX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+          }}
+        />
       </body>
     </html>
   );
