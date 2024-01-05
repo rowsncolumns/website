4c04a45015757bcfb7d8e00261a457c322090be2
Vinay M <rmdort@gmail.com>
2024-01-05 22:08:57 +0000

    updates

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -22,7 +22,7 @@ export function HeaderMenu() {
       </Button>
       <div
         className={cn(
-          "grid row-start-2 col-span-2 lg:col-span-1 lg:row-start-auto lg:grid-cols-[repeat(6,minmax(min-content,max-content))] lg:gap-3 lg:justify-items-end lg:place-content-end lg:!grid",
+          "grid row-start-2 col-span-2 lg:col-span-1 lg:row-start-auto lg:grid-cols-[repeat(7,minmax(min-content,max-content))] lg:gap-1 lg:justify-items-end lg:place-content-end lg:!grid",
           isVisible ? undefined : "hidden"
         )}
       >
@@ -52,11 +52,18 @@ export function HeaderMenu() {
         </Link>
 
         <Link
+          href="https://calendly.com/rowsncolumns/30min"
+          className={buttonVariants({ variant: "ghost" })}
+        >
+          Schedule a call
+        </Link>
+
+        <Link
           href="https://discord.gg/K4cNVh6"
           className={buttonVariants({ variant: "ghost" })}
           target="_blank"
         >
-          Chat
+          Discord
         </Link>
       </div>
     </>
