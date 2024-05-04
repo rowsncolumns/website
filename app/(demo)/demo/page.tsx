c4d8f99297756ba58b56683cdab74984b1d8b43b
Vinay M <rmdort@gmail.com>
2024-05-05 00:19:05 +0100

    #16 Allow selecting cells outside viewport in strict mode

--- app/(demo)/demo/page.tsx
+++ app/(demo)/demo/page.tsx
@@ -11,9 +11,7 @@ export const generateMetadata = (): Metadata => {
 export default async function Demo() {
   return (
     <div className="m-2 flex-1 flex flex-col min-h-0 relative">
-      <div className="h-[400px]">
-        <Spreadsheet />
-      </div>
+      <Spreadsheet />
     </div>
   );
 }
