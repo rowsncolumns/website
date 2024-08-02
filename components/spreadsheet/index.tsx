9f19e261ca55a9438eb4b99cffcfacfde5bad033
Vinay M <rmdort@gmail.com>
2024-08-02 22:12:43 +0100

    Fix issue with onSearch not focusing on active sheet + Fix absolute cell address bug

--- components/spreadsheet/index.tsx
+++ components/spreadsheet/index.tsx
@@ -315,6 +315,9 @@ export const Spreadsheet = () => {
       totalResults,
       searchQuery,
     } = useSearch({
+      sheets,
+      onChangeActiveSheet,
+      searchAllSheets: true,
       getCellData,
       sheetId: activeSheetId,
       getNonEmptyColumnCount,
