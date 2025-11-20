2de4201dcc6c126f950b34d18133fded88e2079b
Vinay M <rmdort@gmail.com>
2025-11-20 22:11:19 +0800

    Make table columns moveable

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,28 +1,29 @@
 import sendMail, { MailDataRequired } from "@sendgrid/mail";
 import { NextResponse } from "next/server";
 
-sendMail.setApiKey(process.env.SENDGRID_API_KEY);
+export const runtime = "nodejs"; // IMPORTANT
+
+const apiKey = process.env.SENDGRID_API_KEY!;
+
+sendMail.setApiKey(apiKey);
 
 export async function POST(request: Request) {
   const { email, message, company, name } = await request.json();
 
   const msg: MailDataRequired = {
     to: "contact@rowsncolumns.app",
-    from: "support@rowsncolumns.app",
+    from: "support@rowsncolumns.app", // MUST BE DOMAIN AUTHENTICATED
     subject: `Contact email - ${name} - ${company}`,
     text: message,
     html: message,
-    replyTo: {
-      name,
-      email,
-    },
+    replyTo: { name, email },
   };
 
   try {
     await sendMail.send(msg);
     return NextResponse.json("Email sent successfully");
-  } catch (error) {
-    console.error(error);
+  } catch (error: any) {
+    console.error("SENDGRID ERROR", error.response?.body || error);
     return NextResponse.json("Error sending email", { status: 500 });
   }
 }
